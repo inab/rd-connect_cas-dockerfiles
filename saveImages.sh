8724a335cdff4e66b8dcbcbef9339596be2f62b5
José María Fernández <jmfernandez@cnio.es>
2017-06-06 20:41:43 +0200

    Last bits of code reordering, so the list of images is taken from the common infrastructure

--- saveImages.sh
+++ saveImages.sh
@@ -2,4 +2,30 @@
 
 set -e
 
-docker save demo_umi demo_pla demo_cas demo_casldap
+if [ $# -gt 0 ] ; then
+	archiveFile="$1"
+	shift
+
+	dockerFileDir="$(dirname "$0")"
+	case "${dockerFileDir}" in
+		/*)
+			true
+			;;
+		*)
+			dockerFileDir="${PWD}"/"${dockerFileDir}"
+			;;
+	esac
+
+	source "${dockerFileDir}"/declDataVolumes.sh.common
+	
+	docker_save_images "${archiveFile}"
+	echo
+	echo "To create the data volumes, run ./initDataVolumes.sh ${origPrefix}"
+	echo "To populate the data volumes, run ./populateDataVolumes.sh ${origPrefix}"
+	echo "To create the instances, run ./createInstances.sh ${origPrefix}"
+	echo "To start the instances, run ./startInstances.sh ${origPrefix}"
+else
+	echo "Usage: $0 {dest_archive}" 1>&2
+	exit 1
+fi
+
