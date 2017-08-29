a29ec09a58721e955f82ff1310f2ad70e92dbe38
José María Fernández <jose.m.fernandez@bsc.es>
2017-08-29 16:13:35 +0200

    Migration process has been improved, as it now allows fine grain property transfers

--- migrateBackupToVolumes.sh
+++ migrateBackupToVolumes.sh
@@ -20,12 +20,13 @@ echo "Using prefix '${prefix}' for data volumes"
 echo "INFO: To drop the generated data volumes, run ./dropDataVolumes.sh '${origPrefix}'"
 
 
-if [ $# -ge 2 ] ; then
+if [ $# -ge 3 ] ; then
 	backupArchive="$1"
 	mappingsFile="$2"
+	transfersFile="$3"
 	
 	# Check whether the file exists
-	if [ -r "${backupArchive}" -a -r "${mappingsFile}" ] ; then
+	if [ -r "${backupArchive}" -a -r "${mappingsFile}" -a -r "${transfersFile}" ] ; then
 		echo "INFO: Testing the integrity of the backup archive"
 		gunzip -t "${backupArchive}"
 		
@@ -48,13 +49,13 @@ if [ $# -ge 2 ] ; then
 		tvol=/tmp/volmnt
 		
 		echo "INFO: Populating data volumes with combined data"
-		docker_migrate_volumes "${prefix}" "${backupArchive}" "${mappingsFile}" "${tvol}"
+		docker_migrate_volumes "${prefix}" "${backupArchive}" "${mappingsFile}" "${transfersFile}" "${tvol}"
 		
 		echo "To create the instances, run ./createInstances.sh ${origPrefix}"
 	else
-		echo "ERROR: backup archive $backupArchive or mappings file ${mappingsFile} do not exist!" 1>&2
+		echo "ERROR: backup archive $backupArchive, mappings file ${mappingsFile} or transfers file ${transfersFile} do not exist!" 1>&2
 		exit 1
 	fi
 else
-	echo "Usage: $0 {migrated volumes prefix} {backup archive} {mappings file}"
+	echo "Usage: $0 {migrated volumes prefix} {backup archive} {mappings file} {transfers file}"
 fi
